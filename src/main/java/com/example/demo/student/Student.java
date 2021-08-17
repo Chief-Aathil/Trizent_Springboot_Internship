1ea356298d5d2d2f9d832ede516fd5a8303a9986
Chief A <aathilr7@gmail.com>
2021-08-17 23:01:20 +0530

    Basic Student class

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,12 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private String name;
+    private Long id;
+    private Integer age;
+    private LocalDate dob;
+    private String email;
+
+}
