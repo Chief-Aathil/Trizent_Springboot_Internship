b63b39df1c43da8906cd2a39da2de38a7ff0dbf6
Chief A <aathilr7@gmail.com>
2021-08-17 23:33:43 +0530

    Basic methods implemented Constructors, Getters, Setters and toString implemented

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -9,4 +9,78 @@ public class Student {
     private LocalDate dob;
     private String email;
 
+    public Student(){}
+
+    public Student(String name,
+                   Long id,
+                   Integer age,
+                   LocalDate dob,
+                   String email) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        this.dob = dob;
+        this.email = email;
+    }
+
+    public Student(String name,
+                   Integer age,
+                   LocalDate dob,
+                   String email) {
+        this.name = name;
+        this.age = age;
+        this.dob = dob;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", age=" + age +
+                ", dob=" + dob +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
