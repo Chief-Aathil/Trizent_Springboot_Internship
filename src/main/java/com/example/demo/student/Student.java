d0abfcffd0617ec3d58e3f3bc3a6c4707e2a62cf
Chief A <aathilr7@gmail.com>
2021-08-18 10:34:46 +0530

    Reformatted constructor arguments order

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -3,16 +3,16 @@ package com.example.demo.student;
 import java.time.LocalDate;
 
 public class Student {
-    private String name;
     private Long id;
+    private String name;
     private Integer age;
     private LocalDate dob;
     private String email;
 
     public Student(){}
 
-    public Student(String name,
-                   Long id,
+    public Student(Long id,
+                   String name,
                    Integer age,
                    LocalDate dob,
                    String email) {
