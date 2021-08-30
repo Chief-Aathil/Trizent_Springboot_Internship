0c8b4a08a5d59509be593edcbcbf6ac0f86f5515
Chief A <aathilr7@gmail.com>
2021-08-30 20:24:19 +0530

    Simple service function

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -2,6 +2,8 @@ package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -23,5 +25,10 @@ public class StudentService {
 
         return studentRepository.findAll();
 
+    }
+
+
+    public void addNewStudent( Student student) {
+        System.out.println(student);
     }
 }
