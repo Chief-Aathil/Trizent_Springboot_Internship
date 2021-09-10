6b14cc30cd418e30f0b1473848cb2cdd5831cf42
Chief A <aathilr7@gmail.com>
2021-09-10 12:59:19 +0530

    Email check. Checking if the email is already present in DB.

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -9,6 +9,7 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -29,6 +30,11 @@ public class StudentService {
 
 
     public void addNewStudent( Student student) {
-        System.out.println(student);
+        Optional<Student> studentOptional= studentRepository.findStudentByEmail(student.getEmail());
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("email taken");
+
+        }
+        studentRepository.save(student);
     }
 }
