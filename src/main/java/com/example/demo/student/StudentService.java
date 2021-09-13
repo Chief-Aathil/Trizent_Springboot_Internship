506f14f2d5842553bf1e3da7e21c43ad05fbe3a7
Chief A <aathilr7@gmail.com>
2021-09-13 13:11:07 +0530

    API to handle PUT request. To update a student entry's name and email.

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -2,13 +2,10 @@ package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 
@@ -46,5 +43,26 @@ public class StudentService {
         }
         studentRepository.deleteById(studentId);
 
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId,String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "student with id "+studentId+"does not exists!"));
+        if(name!=null
+                && name.length()>0
+                && !Objects.equals(student.getName(),name)){
+            student.setName(name);
+        }
+        if(email!=null
+                &&email.length()>0
+                &&!Objects.equals(student.getEmail(),email)){
+            Optional<Student> studentOptional= studentRepository.findStudentByEmail(email);
+            if(studentOptional.isPresent()){
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
