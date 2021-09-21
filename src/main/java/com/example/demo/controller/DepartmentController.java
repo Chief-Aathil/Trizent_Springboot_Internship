88f770e53e0f53074bf354ba292a807a783e9455
Chief A <aathilr7@gmail.com>
2021-09-21 13:22:44 +0530

    Logging using slf4j. to console out.

--- src/main/java/com/example/demo/controller/DepartmentController.java
+++ src/main/java/com/example/demo/controller/DepartmentController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Department;
 import com.example.demo.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,13 +16,17 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+    private final Logger LOGGER =  LoggerFactory.getLogger(DepartmentController.class);
+
     @PostMapping("/departments")
     public Department saveDepartment(@Valid @RequestBody Department department){
+        LOGGER.info("Inside DepartmentController.saveDepartment()");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList(){
+        LOGGER.info("Inside DepartmentController.fetchDepartmentList()");
         return departmentService.fetchDepartmentList();
     }
 
