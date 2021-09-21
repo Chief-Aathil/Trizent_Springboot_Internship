108e8f962493570fdd4511aa43ef3bb1375a3988
Chief A <aathilr7@gmail.com>
2021-09-21 12:29:50 +0530

    departmentName validation

--- src/main/java/com/example/demo/entity/Department.java
+++ src/main/java/com/example/demo/entity/Department.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity // this enables the class to interact with db
 public class Department {
@@ -12,6 +13,8 @@ public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long departmentId;
+
+    @NotBlank(message = "departmentName cannot be blank.")    // for validating name
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
