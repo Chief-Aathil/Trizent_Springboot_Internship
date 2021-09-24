f9a683f21724a807e58ebf08a3ca8e136b9dcbdb
Chief A <aathilr7@gmail.com>
2021-09-24 13:54:37 +0530

    Setup Mocking. for findDepartmentByNameIgnoreCase.

--- src/test/java/com/example/demo/service/DepartmentServiceTest.java
+++ src/test/java/com/example/demo/service/DepartmentServiceTest.java
@@ -1,14 +1,34 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Department;
+import com.example.demo.repository.DepartmentRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class DepartmentServiceTest {
 
+    @Autowired
+    private DepartmentService departmentService;
+
+    @MockBean
+    private DepartmentRepository departmentRepository;
+
     @BeforeEach
     void setUp() {
+        Department department = Department.builder()
+                .departmentId(1L)
+                .departmentName("IT")
+                .departmentAddress("Ahmedabad")
+                .departmentCode("IT-06")
+                .build();
+        Mockito.when(departmentRepository.findByDepartmentNameIgnoreCase("IT"))
+                .thenReturn(department);
     }
 }
