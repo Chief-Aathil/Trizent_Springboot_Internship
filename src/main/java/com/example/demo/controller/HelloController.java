2b36acc90931c972ed64c9202c91e42d9936d991
Chief A <aathilr7@gmail.com>
2021-10-04 13:08:36 +0530

    Welcome message moved to properties file.

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,10 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     //any GET requests with url= <server-address:port>/ will be handled. eg: localhost:8080/
-    @RequestMapping(value = "/",method = RequestMethod.GET)
-    public String helloWorld(){
-        return "Hello World";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String helloWorld() {
+        return welcomeMessage;
     }
 
     //Simplified way to define handlers for GET requests. If you open the annotation you can see that
